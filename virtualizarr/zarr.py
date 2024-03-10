b760f1198b152a66cdda54812871498548b51e14
TomNicholas <tom@cworthy.org>
2024-03-09 20:20:24 -0500

    forgot to add this file in previous commit

---
+++ virtualizarr/zarr.py
@@ -0,0 +1,52 @@
+from typing import Any, Literal, NewType, Optional, Tuple, TypedDict
+
+from pydantic import BaseModel, ConfigDict, validator
+
+import numpy as np
+
+
+# TODO replace these with classes imported directly from Zarr?
+ZAttrs = NewType(
+    "ZAttrs", dict[str, Any]
+)  # just the .zattrs (for one array or for the whole store/group)
+
+
+class ZArray(BaseModel):
+    """Just the .zarray information"""
+
+    # TODO will this work for V3?
+
+    model_config = ConfigDict(
+        frozen=True, 
+        arbitrary_types_allowed=True,  # only here so pydantic doesn't complain about the numpy dtype field
+    )   
+
+    chunks: Tuple[int, ...]
+    compressor: Optional[str]
+    dtype: np.dtype
+    fill_value: Optional[float]  # float or int?
+    filters: Optional[str]
+    order: Literal["C"] | Literal["F"]
+    shape: Tuple[int, ...]
+    zarr_format: Literal[2] | Literal[3]
+
+    @validator('dtype')
+    def validate_dtype(cls, dtype) -> np.dtype:
+        # Your custom validation logic here
+        # Convert numpy.dtype to a format suitable for Pydantic
+        return np.dtype(dtype)
+
+    @classmethod
+    def from_kerchunk_refs(cls, decoded_arr_refs_zarray) -> "ZArray":
+        # TODO should we be doing some type coercion on the 'fill_value' here?
+
+        return ZArray(
+                chunks=tuple(decoded_arr_refs_zarray["chunks"]),
+                compressor=decoded_arr_refs_zarray["compressor"],
+                dtype=np.dtype(decoded_arr_refs_zarray["dtype"]),
+                fill_value=decoded_arr_refs_zarray["fill_value"],
+                filters=decoded_arr_refs_zarray["filters"],
+                order=decoded_arr_refs_zarray["order"],
+                shape=tuple(decoded_arr_refs_zarray["shape"]),
+                zarr_format=int(decoded_arr_refs_zarray["zarr_format"]),
+        )
