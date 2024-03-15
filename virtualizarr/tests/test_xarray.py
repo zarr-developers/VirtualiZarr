775a493364e69047269336d63e3d865820610870
TomNicholas <tom@cworthy.org>
2024-03-14 20:10:03 -0400

    test xarray concatenation along a new dimension

--- virtualizarr/tests/test_xarray.py
+++ virtualizarr/tests/test_xarray.py
@@ -117,3 +117,50 @@ class TestConcat:
         assert result.data.zarray.fill_value == zarray.fill_value
         assert result.data.zarray.order == zarray.order
         assert result.data.zarray.zarr_format == zarray.zarr_format
+
+    def test_concat_along_new_dim(self):
+        # this calls np.stack internally
+        # both manifest arrays in this example have the same zarray properties
+        zarray = ZArray(
+            chunks=(5, 10),
+            compressor="zlib",
+            dtype=np.dtype("int32"),
+            fill_value=0.0,
+            filters=None,
+            order="C",
+            shape=(5, 20),
+            zarr_format=2,
+        )
+
+        chunks_dict1 = {
+            "0.0": {"path": "foo.nc", "offset": 100, "length": 100},
+            "0.1": {"path": "foo.nc", "offset": 200, "length": 100},
+        }
+        manifest1 = ChunkManifest(entries=chunks_dict1)
+        marr1 = ManifestArray(zarray=zarray, chunkmanifest=manifest1)
+        ds1 = xr.Dataset({"a": (["x", "y"], marr1)})
+
+        chunks_dict2 = {
+            "0.0": {"path": "foo.nc", "offset": 300, "length": 100},
+            "0.1": {"path": "foo.nc", "offset": 400, "length": 100},
+        }
+        manifest2 = ChunkManifest(entries=chunks_dict2)
+        marr2 = ManifestArray(zarray=zarray, chunkmanifest=manifest2)
+        ds2 = xr.Dataset({"a": (["x", "y"], marr2)})
+
+        result = xr.concat([ds1, ds2], dim="z")["a"]
+
+        # xarray.concat adds new dimensions along axis=0
+        assert result.shape == (2, 5, 20)
+        assert result.chunks == (1, 5, 10)
+        assert result.data.manifest.dict() == {
+            "0.0.0": {"path": "foo.nc", "offset": 100, "length": 100},
+            "0.0.1": {"path": "foo.nc", "offset": 200, "length": 100},
+            "1.0.0": {"path": "foo.nc", "offset": 300, "length": 100},
+            "1.0.1": {"path": "foo.nc", "offset": 400, "length": 100},
+        }
+        assert result.data.zarray.compressor == zarray.compressor
+        assert result.data.zarray.filters == zarray.filters
+        assert result.data.zarray.fill_value == zarray.fill_value
+        assert result.data.zarray.order == zarray.order
+        assert result.data.zarray.zarr_format == zarray.zarr_format
