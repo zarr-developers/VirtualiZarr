350d806d3cc2267604475320a4841274c1b53bc6
Raphael Hagen <norlandrhagen@gmail.com>
2024-03-17 14:03:43 -0600

    lint + doc.yml & readthedocs.yml

--- virtualizarr/__init__.py
+++ virtualizarr/__init__.py
@@ -1,5 +1,3 @@
 from .manifests import ManifestArray  # type: ignore # noqa
-from .xarray import (  # type: ignore # noqa
-    VirtualiZarrDatasetAccessor,
-    open_dataset_via_kerchunk,
-)
+from .xarray import VirtualiZarrDatasetAccessor  # type: ignore # noqa
+from .xarray import open_dataset_via_kerchunk  # noqa: F401
