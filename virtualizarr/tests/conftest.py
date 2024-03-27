e1957dcd201799fc59a5bba518ad6c3d4270f4b5
Raphael Hagen <norlandrhagen@gmail.com>
2024-03-27 13:18:20 -0600

    created conftest.py and moved two fixtures into conftest

---
+++ virtualizarr/tests/conftest.py
@@ -0,0 +1,31 @@
+import pytest
+import xarray as xr 
+
+@pytest.fixture
+def netcdf4_file(tmpdir):
+    # Set up example xarray dataset
+    ds = xr.tutorial.open_dataset("air_temperature")
+
+    # Save it to disk as netCDF (in temporary directory)
+    filepath = f"{tmpdir}/air.nc"
+    ds.to_netcdf(filepath)
+
+    return filepath
+
+
+@pytest.fixture
+def netcdf4_files(tmpdir):
+    # Set up example xarray dataset
+    ds = xr.tutorial.open_dataset("air_temperature")
+
+    # split inrto equal chunks so we can concatenate them back together later
+    ds1 = ds.isel(time=slice(None, 1460))
+    ds2 = ds.isel(time=slice(1460, None))
+
+    # Save it to disk as netCDF (in temporary directory)
+    filepath1 = f"{tmpdir}/air1.nc"
+    filepath2 = f"{tmpdir}/air2.nc"
+    ds1.to_netcdf(filepath1)
+    ds2.to_netcdf(filepath2)
+
+    return filepath1, filepath2
--- virtualizarr/tests/test_xarray.py
+++ virtualizarr/tests/test_xarray.py
@@ -225,34 +225,7 @@ class TestConcat:
         assert result.data.zarray.zarr_format == zarray.zarr_format
 
 
-@pytest.fixture
-def netcdf4_file(tmpdir):
-    # Set up example xarray dataset
-    ds = xr.tutorial.open_dataset("air_temperature")
 
-    # Save it to disk as netCDF (in temporary directory)
-    filepath = f"{tmpdir}/air.nc"
-    ds.to_netcdf(filepath)
-
-    return filepath
-
-
-@pytest.fixture
-def netcdf4_files(tmpdir):
-    # Set up example xarray dataset
-    ds = xr.tutorial.open_dataset("air_temperature")
-
-    # split inrto equal chunks so we can concatenate them back together later
-    ds1 = ds.isel(time=slice(None, 1460))
-    ds2 = ds.isel(time=slice(1460, None))
-
-    # Save it to disk as netCDF (in temporary directory)
-    filepath1 = f"{tmpdir}/air1.nc"
-    filepath2 = f"{tmpdir}/air2.nc"
-    ds1.to_netcdf(filepath1)
-    ds2.to_netcdf(filepath2)
-
-    return filepath1, filepath2
 
 
 class TestOpenVirtualDatasetIndexes:
