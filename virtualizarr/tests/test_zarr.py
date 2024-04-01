98a259edab8e8e912aa2f6496dd59cddee6b3bf5
TomNicholas <tom@cworthy.org>
2024-04-01 16:11:33 -0400

    forgot to add the file with the test

---
+++ virtualizarr/tests/test_zarr.py
@@ -0,0 +1,27 @@
+import xarray as xr
+import numpy as np
+import xarray.testing as xrt
+from virtualizarr import open_virtual_dataset, ManifestArray
+from virtualizarr.manifests.manifest import ChunkEntry
+
+
+def test_zarr_v3_roundtrip(tmpdir):
+    arr = ManifestArray(
+            chunkmanifest={"0.0": ChunkEntry(path="test.nc", offset=6144, length=48)},
+            zarray=dict(
+                shape=(2, 3),
+                dtype=np.dtype("<i8"),
+                chunks=(2, 3),
+                compressor=None,
+                filters=None,
+                fill_value=None,
+                order="C",
+                zarr_format=3,
+            ),
+        )
+    original = xr.Dataset({"a": (["x", "y"], arr)})
+
+    original.virtualize.to_zarr(tmpdir / "store.zarr")
+    roundtrip = open_virtual_dataset(tmpdir / "store.zarr", filetype="zarr_v3", indexes={})
+
+    xrt.assert_identical(roundtrip, original)
