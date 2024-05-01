e014bb78ace0ef285e6dbe1032196fabe28b6221
Tom Nicholas <tom@cworthy.org>
2024-05-01 15:04:10 -0600

    Write manifests to zarr store (#45)
    * basic structure for writing a zarr store containing manifests
    
    * write in nicely indented form
    
    * use pathlib for everything
    
    * documentation
    
    * docstrings
    
    * vendor zarr.utils.json_dumps
    
    * remove consolidated metadata, as v3 doesn't have this yet
    
    * license for vendoring part of zarr-python
    
    * change to write v3
    
    * implement reading from v3-compliant stores
    
    * roundtripping test
    
    * forgot to add the file with the test
    
    * test dataset-level attributes
    
    * debugging print
    
    * try explicitly separating files from directories

--- virtualizarr/zarr.py
+++ virtualizarr/zarr.py
@@ -1,8 +1,16 @@
-from typing import Any, Literal, NewType, Optional, Tuple, Union, List, Dict
+
+from pathlib import Path
+from typing import Any, Literal, NewType, Optional, Tuple, Union, List, Dict, TYPE_CHECKING
+import json
 
 import numpy as np
 import ujson  # type: ignore
+import xarray as xr
 from pydantic import BaseModel, ConfigDict, field_validator
+from virtualizarr.vendor.zarr.utils import json_dumps
+
+if TYPE_CHECKING:
+    pass
 
 # TODO replace these with classes imported directly from Zarr? (i.e. Zarr Object Models)
 ZAttrs = NewType(
@@ -82,11 +90,18 @@ class ZArray(BaseModel):
             zarr_format=int(decoded_arr_refs_zarray["zarr_format"]),
         )
 
-    def to_kerchunk_json(self) -> str:
+    def dict(self) -> dict[str, Any]:
         zarray_dict = dict(self)
-        # TODO not sure if there is a better way to get the '<i4' style representation of the dtype out
-        zarray_dict["dtype"] = zarray_dict["dtype"].descr[0][1]
-        return ujson.dumps(zarray_dict)
+        zarray_dict["dtype"] = encode_dtype(zarray_dict["dtype"])
+        return zarray_dict
+
+    def to_kerchunk_json(self) -> str:
+        return ujson.dumps(self.dict())
+
+
+def encode_dtype(dtype: np.dtype) -> str:
+    # TODO not sure if there is a better way to get the '<i4' style representation of the dtype out
+    return dtype.descr[0][1]
 
 
 def ceildiv(a: int, b: int) -> int:
@@ -96,3 +111,149 @@ def ceildiv(a: int, b: int) -> int:
     See https://stackoverflow.com/questions/14822184/is-there-a-ceiling-equivalent-of-operator-in-python
     """
     return -(a // -b)
+
+
+def dataset_to_zarr(ds: xr.Dataset, storepath: str) -> None:
+    """
+    Write an xarray dataset whose variables wrap ManifestArrays to a v3 Zarr store, writing chunk references into manifest.json files.
+
+    Currently requires all variables to be backed by ManifestArray objects.
+
+    Not very useful until some implementation of a Zarr reader can actually read these manifest.json files.
+    See https://github.com/zarr-developers/zarr-specs/issues/287
+
+    Parameters
+    ----------
+    ds: xr.Dataset
+    storepath: str
+    """
+
+    from virtualizarr.manifests import ManifestArray
+
+    _storepath = Path(storepath)
+    Path.mkdir(_storepath, exist_ok=False)
+
+    # should techically loop over groups in a tree but a dataset corresponds to only one group
+    group_metadata = {
+        "zarr_format": 3,
+        "node_type": "group",
+        "attributes": ds.attrs
+    }
+    with open(_storepath / 'zarr.json', "wb") as group_metadata_file:
+        group_metadata_file.write(json_dumps(group_metadata))
+
+    for name, var in ds.variables.items():
+        array_dir = _storepath / name
+        marr = var.data
+
+        # TODO move this check outside the writing loop so we don't write an incomplete store on failure?
+        # TODO at some point this should be generalized to also write in-memory arrays as normal zarr chunks, see GH isse #62.
+        if not isinstance(marr, ManifestArray):
+            raise TypeError(
+                "Only xarray objects wrapping ManifestArrays can be written to zarr using this method, "
+                f"but variable {name} wraps an array of type {type(marr)}"
+            )
+
+        Path.mkdir(array_dir, exist_ok=False)
+
+        # write the chunk references into a manifest.json file
+        # and the array metadata into a zarr.json file
+        to_zarr_json(var, array_dir)
+
+
+def to_zarr_json(var: xr.Variable, array_dir: Path) -> None:
+    """
+    Write out both the zarr.json and manifest.json file into the given zarr array directory.
+
+    Follows the Zarr v3 manifest storage transformer ZEP (see https://github.com/zarr-developers/zarr-specs/issues/287).
+
+    Parameters
+    ----------
+    var : xr.Variable
+        Must be wrapping a ManifestArray
+    dirpath : str
+        Zarr store array directory into which to write files.
+    """
+
+    marr = var.data
+
+    marr.manifest.to_zarr_json(array_dir / 'manifest.json')
+
+    metadata = zarr_v3_array_metadata(marr.zarray, list(var.dims), var.attrs)
+    with open(array_dir / 'zarr.json', "wb") as metadata_file:
+        metadata_file.write(json_dumps(metadata))
+
+
+def zarr_v3_array_metadata(zarray: ZArray, dim_names: List[str], attrs: dict) -> dict:
+    """Construct a v3-compliant metadata dict from v2 zarray + information stored on the xarray variable."""
+    # TODO it would be nice if we could use the zarr-python metadata.ArrayMetadata classes to do this conversion for us
+
+    metadata = zarray.dict()
+
+    # adjust to match v3 spec
+    metadata["zarr_format"] = 3
+    metadata["node_type"] = "array"
+    metadata["data_type"] = str(np.dtype(metadata.pop("dtype")))
+    metadata["chunk_grid"] = {"name": "regular", "configuration": {"chunk_shape": metadata.pop("chunks")}}
+    metadata["chunk_key_encoding"] = {
+        "name": "default",
+        "configuration": {
+            "separator": "/"
+        }
+    }
+    metadata["codecs"] = metadata.pop("filters")
+    metadata.pop("compressor")  # TODO this should be entered in codecs somehow
+    metadata.pop("order")  # TODO this should be replaced by a transpose codec
+
+    # indicate that we're using the manifest storage transformer ZEP
+    metadata["storage_transformers"] = [
+        {
+            "name": "chunk-manifest-json",
+            "configuration": {
+                "manifest": "./manifest.json"
+            }
+        }
+    ]
+
+    # add information from xarray object
+    metadata["dimension_names"] = dim_names
+    metadata["attributes"] = attrs
+
+    return metadata
+
+
+def attrs_from_zarr_group_json(filepath: Path) -> dict:
+    with open(filepath, "r") as metadata_file:
+        attrs = json.load(metadata_file)
+    return attrs["attributes"]
+
+
+def metadata_from_zarr_json(filepath: Path) -> Tuple[ZArray, List[str], dict]:
+    with open(filepath, "r") as metadata_file:
+        metadata = json.load(metadata_file)
+
+    if {
+            "name": "chunk-manifest-json",
+            "configuration": {
+                "manifest": "./manifest.json",
+            }
+       } not in metadata.get("storage_transformers", []):
+        raise ValueError("Can only read byte ranges from Zarr v3 stores which implement the manifest storage transformer ZEP.")
+
+    attrs = metadata.pop("attributes")
+    dim_names = metadata.pop("dimension_names")
+
+    chunk_shape = metadata["chunk_grid"]["configuration"]["chunk_shape"]
+
+    zarray = ZArray(
+        chunks=metadata["chunk_grid"]["configuration"]["chunk_shape"],
+        compressor=metadata["codecs"],
+        dtype=np.dtype(metadata["data_type"]),
+        fill_value=metadata["fill_value"],
+        filters=metadata.get("filters", None),
+        order="C",
+        shape=chunk_shape,
+        zarr_format=3,
+    )
+
+    return zarray, dim_names, attrs
