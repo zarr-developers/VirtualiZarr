7c02f19b3e9413ba37e8aa280d3f16d8c7601c10
TomNicholas <tom@cworthy.org>
2024-05-03 11:46:13 -0400

    parametrize test to test inlining different variables

--- virtualizarr/tests/test_integration.py
+++ virtualizarr/tests/test_integration.py
@@ -1,16 +1,21 @@
+import pytest
+
 from virtualizarr import open_virtual_dataset
 
 
-def test_numpy_arrays_to_inlined_kerchunk_refs(netcdf4_file):
+@pytest.mark.parametrize("inline_threshold, vars_to_inline", [
+    (5e2, ['lat', 'lon']),
+    pytest.param(5e4, ['lat', 'lon', 'time'], marks=pytest.mark.xfail(reason='time encoding')),
+    pytest.param(5e7, ['lat', 'lon', 'time', 'air'], marks=pytest.mark.xfail(reason='scale factor encoding')),
+])
+def test_numpy_arrays_to_inlined_kerchunk_refs(netcdf4_file, inline_threshold, vars_to_inline):
     from kerchunk.hdf import SingleHdf5ToZarr
 
-    # test inlining all the variables
-    expected = SingleHdf5ToZarr(netcdf4_file, spec=1, inline_threshold=500).translate()
+    # inline_threshold is chosen to test inlining only the variables listed in vars_to_inline
+    expected = SingleHdf5ToZarr(netcdf4_file, spec=1, inline_threshold=inline_threshold).translate()
 
-    # loading all the variables should produce same result as inlining them all using kerchunk
-
-    # TODO test the time variable too
-    vds = open_virtual_dataset(netcdf4_file, loadable_variables=['lat', 'lon'], indexes={})
+    # loading the variables should produce same result as inlining them using kerchunk
+    vds = open_virtual_dataset(netcdf4_file, loadable_variables=vars_to_inline, indexes={})
     refs = vds.virtualize.to_kerchunk(format='dict')
 
     # TODO I would just compare the entire dicts but kerchunk returns inconsistent results - see https://github.com/TomNicholas/VirtualiZarr/pull/73#issuecomment-2040931202
