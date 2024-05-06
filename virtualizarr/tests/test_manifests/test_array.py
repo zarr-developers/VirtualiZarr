c026d0a25ed1ea59f8f934b33bf55ea2e675f898
TomNicholas <tom@cworthy.org>
2024-05-06 18:37:47 -0400

    test creating scalars from kerchunk refs

--- virtualizarr/tests/test_manifests/test_array.py
+++ virtualizarr/tests/test_manifests/test_array.py
@@ -34,27 +34,6 @@ class TestManifestArray:
         assert marr.size == 5 * 2 * 20
         assert marr.ndim == 3
 
-    def test_create_invalid_manifestarray(self):
-        chunks_dict = {
-            "0.0.0": {"path": "foo.nc", "offset": 100, "length": 100},
-        }
-        manifest = ChunkManifest(entries=chunks_dict)
-        chunks = (5, 1, 10)
-        shape = (5, 2, 20)
-        zarray = ZArray(
-            chunks=chunks,
-            compressor="zlib",
-            dtype=np.dtype("int32"),
-            fill_value=0.0,
-            filters=None,
-            order="C",
-            shape=shape,
-            zarr_format=2,
-        )
-
-        with pytest.raises(ValueError, match="Inconsistent chunk grid shape"):
-            ManifestArray(zarray=zarray, chunkmanifest=manifest)
-
     def test_create_manifestarray_from_kerchunk_refs(self):
         arr_refs = {
             ".zarray": '{"chunks":[2,3],"compressor":null,"dtype":"<i8","fill_value":null,"filters":null,"order":"C","shape":[2,3],"zarr_format":2}',
@@ -69,6 +48,16 @@ class TestManifestArray:
         assert marr.zarray.fill_value is None
         assert marr.zarray.filters is None
         assert marr.zarray.order == "C"
+
+    def test_create_scalar_manifestarray_from_kerchunk_refs(self):
+        arr_refs = {
+            ".zarray": '{"chunks":[],"compressor":null,"dtype":"<i8","fill_value":null,"filters":null,"order":"C","shape":[],"zarr_format":2}',
+            "0": ["test1.nc", 6144, 48],
+        }
+        marr = ManifestArray._from_kerchunk_refs(arr_refs)
+
+        assert marr.shape == ()
+        assert marr.chunks == ()
 
 
 class TestEquals:
