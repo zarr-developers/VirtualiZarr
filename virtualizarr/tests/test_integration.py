d0ea94ff03430c055d755066bd28d5c3f647f00e
TomNicholas <tom@cworthy.org>
2024-05-07 13:37:20 -0400

    use temporary directory

--- virtualizarr/tests/test_integration.py
+++ virtualizarr/tests/test_integration.py
@@ -2,11 +2,11 @@ import xarray as xr
 from virtualizarr import open_virtual_dataset
 
 
-def test_open_scalar_variable():
-    # regression test for issue 100
+def test_open_scalar_variable(tmpdir):
+    # regression test for GH issue #100
 
     ds = xr.Dataset(data_vars={'a': 0})
-    ds.to_netcdf('scalar.nc')
+    ds.to_netcdf(f"{tmpdir}/scalar.nc")
 
-    vds = open_virtual_dataset('scalar.nc')
+    vds = open_virtual_dataset(f"{tmpdir}/scalar.nc")
     assert vds['a'].shape == ()
