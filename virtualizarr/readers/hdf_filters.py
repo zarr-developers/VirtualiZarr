c57380058a5ad6ddbd908d54b1edd85b1f74f91d
sharkinsspatial <sharkinsgis@gmail.com>
2024-05-19 16:12:50 -0600

    Fix mypy complaints for hdf_filters.

--- virtualizarr/readers/hdf_filters.py
+++ virtualizarr/readers/hdf_filters.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple, Union
+from typing import List, Optional, Tuple, TypedDict, Union
 
 import h5py
 import hdf5plugin
@@ -25,26 +25,30 @@ class BloscProperties(BaseModel):
         return blosc_compressor_codes[v]
 
 
-def _filter_to_codec(filter_id: str, filter_properties: Union[int, Tuple] = None) -> Codec:
+def _filter_to_codec(filter_id: str, filter_properties: Union[int, None, Tuple] = None) -> Codec:
+    id_int = None
+    id_str = None
     try:
-        id = int(filter_id)
+        id_int = int(filter_id)
     except ValueError:
-        id = filter_id
+        id_str = filter_id
 
-    if isinstance(id, str):
-        if id in _non_standard_filters.keys():
-            id = _non_standard_filters[id]
+    if id_str:
+        if id_str in _non_standard_filters.keys():
+            id = _non_standard_filters[id_str]
+        else:
+            id = id_str
         conf = {"id": id}
         if id == "zlib":
-            conf["level"] = filter_properties
-    elif isinstance(id, int):
-        filter = hdf5plugin.get_filters(id)[0]
+            conf["level"] = filter_properties # type: ignore[assignment]
+    if id_int:
+        filter = hdf5plugin.get_filters(id_int)[0]
         id = filter.filter_name
-        if id == "blosc":
+        if id == "blosc" and isinstance(filter_properties, tuple):
             blosc_props = BloscProperties(**{k: v for k, v in
                                              zip(BloscProperties.__fields__.keys(),
                                                  filter_properties[-4:])})
-            conf = blosc_props.model_dump()
+            conf = blosc_props.model_dump() # type: ignore[assignment]
             conf["id"] = id
 
     codec = registry.get_codec(conf)
