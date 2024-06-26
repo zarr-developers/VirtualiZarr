ac40241cbb07d58c5f81663e75cf5f191ea90c6b
TomNicholas <tom@cworthy.org>
2024-06-26 16:41:25 -0400

    parametrize the test over different filetypes

--- virtualizarr/tests/test_xarray.py
+++ virtualizarr/tests/test_xarray.py
@@ -308,29 +308,29 @@ class TestReadFromS3:
             assert isinstance(vds[var].data, ManifestArray), var
 
 
+EXAMPLE_URLS = {
+    "grib": "https://github.com/pydata/xarray-data/raw/master/era5-2mt-2019-03-uk.grib",
+    "netcdf3": "https://github.com/pydata/xarray-data/raw/master/air_temperature.nc",
+    "netcdf4": "https://github.com/pydata/xarray-data/raw/master/ROMS_example.nc",
+    "hdf4": "https://github.com/corteva/rioxarray/raw/master/test/test_data/input/MOD09GA.A2008296.h14v17.006.2015181011753.hdf",
+    # https://github.com/zarr-developers/VirtualiZarr/issues/159
+    # "hdf5": "https://github.com/fsspec/kerchunk/raw/main/kerchunk/tests/NEONDSTowerTemperatureData.hdf5",
+    "tiff": "https://github.com/fsspec/kerchunk/raw/main/kerchunk/tests/lcmap_tiny_cog_2020.tif",
+    # "fits": "https://fits.gsfc.nasa.gov/samples/WFPC2u5780205r_c0fx.fits",
+    "jpg": "https://github.com/rasterio/rasterio/raw/main/tests/data/389225main_sw_1965_1024.jpg",
+}
+
+
 @network
 class TestReadFromURL:
-    def test_read_from_url(self):
-        examples = {
-            "grib": "https://github.com/pydata/xarray-data/raw/master/era5-2mt-2019-03-uk.grib",
-            "netcdf3": "https://github.com/pydata/xarray-data/raw/master/air_temperature.nc",
-            "netcdf4": "https://github.com/pydata/xarray-data/raw/master/ROMS_example.nc",
-            "hdf4": "https://github.com/corteva/rioxarray/raw/master/test/test_data/input/MOD09GA.A2008296.h14v17.006.2015181011753.hdf",
-            # https://github.com/zarr-developers/VirtualiZarr/issues/159
-            # "hdf5": "https://github.com/fsspec/kerchunk/raw/main/kerchunk/tests/NEONDSTowerTemperatureData.hdf5",
-            # https://github.com/zarr-developers/VirtualiZarr/issues/160
-            "tiff": "https://github.com/fsspec/kerchunk/raw/main/kerchunk/tests/lcmap_tiny_cog_2020.tif",
-            # "fits": "https://fits.gsfc.nasa.gov/samples/WFPC2u5780205r_c0fx.fits",
-            "jpg": "https://github.com/rasterio/rasterio/raw/main/tests/data/389225main_sw_1965_1024.jpg",
-        }
-
-        for filetype, url in examples.items():
-            if filetype in ["grib", "jpg", "hdf4"]:
-                with pytest.raises(NotImplementedError):
-                    vds = open_virtual_dataset(url, reader_options={}, indexes={})
-            else:
+    @pytest.mark.parametrize("filetype, url", [(k, v) for k, v in EXAMPLE_URLS.items()])
+    def test_read_from_url(self, filetype, url):
+        if filetype in ["grib", "jpg", "hdf4"]:
+            with pytest.raises(NotImplementedError):
                 vds = open_virtual_dataset(url, reader_options={}, indexes={})
-                assert isinstance(vds, xr.Dataset)
+        else:
+            vds = open_virtual_dataset(url, reader_options={}, indexes={})
+            assert isinstance(vds, xr.Dataset)
 
 
 class TestLoadVirtualDataset:
