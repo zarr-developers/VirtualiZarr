d352104393d0eeacfc3b566a9f0cb79c7e688c8f
sharkinsspatial <sharkinsgis@gmail.com>
2024-06-30 13:07:17 -0600

    Use decode_times for integration test.

--- virtualizarr/tests/test_readers/test_hdf_integration.py
+++ virtualizarr/tests/test_readers/test_hdf_integration.py
@@ -1,4 +1,3 @@
-import pytest
 import xarray as xr
 import xarray.testing as xrt
 
@@ -7,19 +6,18 @@ from virtualizarr.kerchunk import FileType
 
 
 class TestIntegration:
-    @pytest.mark.xfail(reason="Investigate initial time value decoding issue")
     def test_filters_h5netcdf_roundtrip(
         self, tmpdir, filter_encoded_xarray_h5netcdf_file
     ):
-        ds = xr.open_dataset(filter_encoded_xarray_h5netcdf_file, decode_times=False)
+        ds = xr.open_dataset(filter_encoded_xarray_h5netcdf_file, decode_times=True)
         vds = virtualizarr.open_virtual_dataset(
-            filter_encoded_xarray_h5netcdf_file, filetype=FileType("netcdf4")
+            filter_encoded_xarray_h5netcdf_file,
+            loadable_variables=["time"],
+            cftime_variables=["time"],
         )
         kerchunk_file = f"{tmpdir}/kerchunk.json"
         vds.virtualize.to_kerchunk(kerchunk_file, format="json")
-        roundtrip = xr.open_dataset(
-            kerchunk_file, engine="kerchunk", decode_times=False
-        )
+        roundtrip = xr.open_dataset(kerchunk_file, engine="kerchunk", decode_times=True)
         xrt.assert_allclose(ds, roundtrip)
 
     def test_filters_netcdf4_roundtrip(
