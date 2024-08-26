05a9ea7eb5a7a7d28af8582d1a09a98352e120c9
TomNicholas <tom@cworthy.org>
2024-08-26 19:07:26 -0400

    absolute imports in top-level init

--- virtualizarr/__init__.py
+++ virtualizarr/__init__.py
@@ -1,6 +1,6 @@
-from .manifests import ChunkManifest, ManifestArray  # type: ignore # noqa
-from .accessor import VirtualiZarrDatasetAccessor  # type: ignore # noqa
-from .backend import open_virtual_dataset  # noqa: F401
+from virtualizarr.manifests import ChunkManifest, ManifestArray  # type: ignore # noqa
+from virtualizarr.accessor import VirtualiZarrDatasetAccessor  # type: ignore # noqa
+from virtualizarr.backend import open_virtual_dataset  # noqa: F401
 
 from importlib.metadata import version as _version
 
