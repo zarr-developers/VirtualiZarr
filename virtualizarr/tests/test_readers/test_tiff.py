e39072e813b9bf668f874770b823aa9e46130afc
TomNicholas <tom@cworthy.org>
2024-11-08 19:09:53 -0500

    basic test for tiff reader

---
+++ virtualizarr/tests/test_readers/test_tiff.py
@@ -0,0 +1,26 @@
+from xarray import Dataset
+import numpy as np
+
+from virtualizarr.tests import requires_pillow
+from virtualizarr import open_virtual_dataset
+from virtualizarr.manifests import ManifestArray
+
+
+@requires_pillow
+def test_random_tiff(random_tiff):
+    vds = open_virtual_dataset(random_tiff, indexes={})
+
+    assert isinstance(vds, Dataset)
+
+    # TODO what is the name of this array expected to be??    
+    assert list(vds.variables) == ["foo"]
+    vda = vds["foo"]
+
+    assert vda.sizes == {"X": 128, "Y": 128}
+    assert vda.dtype == np.uint8
+
+    assert isinstance(vda.data, ManifestArray)
+    manifest = vda.data.manifest
+    assert manifest.dict() == {
+        '0.0': {'path': random_tiff, 'offset': 122, 'length': 16384} 
+    }
