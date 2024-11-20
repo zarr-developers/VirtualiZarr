c36962df8de8702a5afd3144c4a74da3f5c9281f
Raphael Hagen <norlandrhagen@gmail.com>
2024-11-19 18:48:24 -0700

    add more tests

--- virtualizarr/tests/test_readers/test_zarr.py
+++ virtualizarr/tests/test_readers/test_zarr.py
@@ -1,6 +1,7 @@
 import pytest
 import zarr
 
+from virtualizarr.manifests import ManifestArray
 from virtualizarr.readers.zarr import virtual_dataset_from_zarr_group
 from virtualizarr.tests import network, requires_zarrV3
 
@@ -15,7 +16,7 @@ from virtualizarr.tests import network, requires_zarrV3
             3,
             id="Zarr V3",
             marks=pytest.mark.skip(
-                reason="Need to translate metadata naming conventions/transforms"
+                reason="ToDo/WIP: Need to translate metadata naming conventions/transforms"
             ),
         ),
     ],
@@ -26,7 +27,21 @@ def test_dataset_from_zarr(zarr_store):
     vds = virtual_dataset_from_zarr_group(filepath=zarr_store, indexes={})
 
     zg_metadata_dict = zg.metadata.to_dict()
+    non_var_arrays = ["time", "lat", "lon"]
+    # check dims and coords are present
+    assert set(vds.coords) == set(non_var_arrays)
+    assert set(vds.dims) == set(non_var_arrays)
+    # check vars match
+    assert set(vds.keys()) == set(["air"])
 
+    # arrays are ManifestArrays
+    for array in list(vds):
+        assert isinstance(vds[array].data, ManifestArray)
+
+    # check top level attrs
+    assert zg.attrs.asdict() == vds.attrs
+
+    # check ZArray values
     arrays = [val for val in zg.keys()]
     zarray_checks = [
         "shape",
@@ -37,6 +52,10 @@ def test_dataset_from_zarr(zarr_store):
         "filters",
         "zarr_format",
     ]  # "dtype"
+
+    # Failure! fill value from zarr is None, None: ipdb> np.dtype(None): dtype('float64') is coerced in zarr.py L21 to 0.0.
+    # assert vds[array].data.zarray.fill_value == zg_metadata_dict['consolidated_metadata']['metadata'][array]['fill_value']
+
     # loop through each array and check ZArray info
     for array in arrays:
         for attr in zarray_checks:
@@ -44,6 +63,3 @@ def test_dataset_from_zarr(zarr_store):
                 getattr(vds[array].data.zarray, attr)
                 == zg_metadata_dict["consolidated_metadata"]["metadata"][array][attr]
             )
-
-        # Failure! fill value from zarr is None, None: ipdb> np.dtype(None): dtype('float64') is coerced in zarr.py L21 to 0.0.
-        # assert vds[array].data.zarray.fill_value == zg_metadata_dict['consolidated_metadata']['metadata'][array]['fill_value']
