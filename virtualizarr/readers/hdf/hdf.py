515262493521a3d0a0659d8290ae28e0797faf47
Tom Nicholas <tom@cworthy.org>
2024-11-27 11:00:10 -0700

    Add reader_kwargs argument to open_virtual_dataset (#315)
    * add reader_kwargs argument to open_virtual_dataset, and pass it down to every reader
    
    * rename reader_kwargs -> virtual_backend_kwargs
    
    * release note

--- virtualizarr/readers/hdf/hdf.py
+++ virtualizarr/readers/hdf/hdf.py
@@ -38,8 +38,14 @@ class HDFVirtualBackend(VirtualBackend):
         loadable_variables: Iterable[str] | None = None,
         decode_times: bool | None = None,
         indexes: Mapping[str, Index] | None = None,
+        virtual_backend_kwargs: Optional[dict] = None,
         reader_options: Optional[dict] = None,
     ) -> xr.Dataset:
+        if virtual_backend_kwargs:
+            raise NotImplementedError(
+                "HDF reader does not understand any virtual_backend_kwargs"
+            )
+
         drop_variables, loadable_variables = check_for_collisions(
             drop_variables,
             loadable_variables,
