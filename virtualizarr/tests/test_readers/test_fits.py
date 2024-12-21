b5f35a907eb39348757a0ac3acf28a8ad98a7318
TomNicholas <tom@cworthy.org>
2024-12-21 12:31:07 -0500

    add test for FITS reader that checks if it can open Hubble data on AWS correctly

---
+++ virtualizarr/tests/test_readers/test_fits.py
@@ -0,0 +1,25 @@
+import pytest
+
+from xarray import Dataset
+
+from virtualizarr import open_virtual_dataset
+from virtualizarr.tests import network, requires_kerchunk
+
+
+pytest.importorskip("astropy")
+
+
+@requires_kerchunk
+@network
+def test_open_hubble_data():
+    # data from https://registry.opendata.aws/hst/
+    vds = open_virtual_dataset(
+        "s3://stpubdata/hst/public/f05i/f05i0201m/f05i0201m_a1f.fits",
+        reader_options={'storage_options': {'anon': True}}
+    )
+    
+    assert isinstance(vds, Dataset)
+    assert list(vds.variables) == ['PRIMARY']
+    var = vds['PRIMARY'].variable
+    assert var.sizes == {'y': 17, 'x': 589}
+    assert var.dtype == '>i4'
