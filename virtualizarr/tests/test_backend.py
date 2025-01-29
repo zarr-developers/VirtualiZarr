e18e6473fa79a5d5703357fb05f8a31f9cef90ac
Julia Signell <jsignell@gmail.com>
2025-01-29 15:42:01 -0500

    Encode #401 behavior in tests

--- virtualizarr/tests/test_backend.py
+++ virtualizarr/tests/test_backend.py
@@ -188,6 +188,8 @@ class TestDetermineCoords:
             + expected_2d_coords
             + expected_1d_non_dimension_coords
             + expected_scalar_coords
+            # These should not be included in coords see #401 for more information
+            + (["xi_rho", "eta_rho"] if hdf_backend == HDFVirtualBackend else [])
         )
         assert set(vds.coords) == set(expected_coords)
 
@@ -336,7 +338,10 @@ class TestOpenVirtualDatasetHDFGroup:
             indexes={},
             backend=hdf_backend,
         )
-        assert list(vds.variables) == ["bar"]
+        # This should just be ["bar"] see #401 for more information
+        assert list(vds.variables) == (
+            ["bar", "dim_0"] if hdf_backend == HDFVirtualBackend else ["bar"]
+        )
         assert isinstance(vds["bar"].data, ManifestArray)
         assert vds["bar"].shape == (2,)
 
@@ -349,7 +354,10 @@ class TestOpenVirtualDatasetHDFGroup:
             indexes={},
             backend=hdf_backend,
         )
-        assert list(vds.variables) == ["foo"]
+        # This should just be ["foo"] see #401 for more information
+        assert list(vds.variables) == (
+            ["foo", "dim_0"] if hdf_backend == HDFVirtualBackend else ["foo"]
+        )
         assert isinstance(vds["foo"].data, ManifestArray)
         assert vds["foo"].shape == (3,)
 
@@ -361,7 +369,10 @@ class TestOpenVirtualDatasetHDFGroup:
             indexes={},
             backend=hdf_backend,
         )
-        assert list(vds.variables) == ["foo"]
+        # This should just be ["foo"] see #401 for more information
+        assert list(vds.variables) == (
+            ["foo", "dim_0"] if hdf_backend == HDFVirtualBackend else ["foo"]
+        )
         assert isinstance(vds["foo"].data, ManifestArray)
         assert vds["foo"].shape == (3,)
 
