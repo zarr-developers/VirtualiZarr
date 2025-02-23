99f3f6a4846e1429084c7e4351b6c0834c3b6ec4
Aimee Barciauskas <aimee@developmentseed.org>
2025-02-22 20:06:36 -0500

    Drafted workflow

--- examples/mursst-icechunk-with-lithops/test_lithops.py
+++ examples/mursst-icechunk-with-lithops/test_lithops.py
@@ -1,12 +1,13 @@
-import icechunk
 import lithops
+
+import virtualizarr
 
 fexec = lithops.FunctionExecutor(config_file="lithops.yaml")
 
 
 def test_function(args: dict):
-    print(icechunk.__version__)
+    return virtualizarr.__version__
 
 
-result = fexec.call_async(func=test_function, data={"args": {}})  # .get_result()
+result = fexec.call_async(func=test_function, data={"args": {}}).result()
 print(result)
