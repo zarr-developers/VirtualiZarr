4488d79a557a6a15a5cad04e0998bf3311ce9a48
Aimee Barciauskas <aimee@developmentseed.org>
2025-02-26 10:21:43 -0800

    Working on uv and cli arguments

--- examples/mursst-icechunk-with-lithops/lithops_functions.py
+++ examples/mursst-icechunk-with-lithops/lithops_functions.py
@@ -22,10 +22,18 @@ fs_read = fsspec.filesystem("s3", anon=False, skip_instance_cache=True)
 base_url = "s3://podaac-ops-cumulus-protected/MUR-JPL-L4-GLOB-v4.1"
 data_vars = ["analysed_sst", "analysis_error", "mask", "sea_ice_fraction"]
 drop_vars = ["dt_1km_data", "sst_anomaly"]
-bucket = "nasa-veda-scratch"
-store_name = "MUR-JPL-L4-GLOB-v4.1-virtual-v5"
+bucket = "nasa-eodc-scratch"
+store_name = "MUR-JPL-L4-GLOB-v4.1-virtual-v1"
 lat_slice = slice(48.5, 48.7)
 lon_slice = slice(-124.7, -124.5)
+
+fexec = lithops.FunctionExecutor(config_file="lithops.yaml", log_level=logging.INFO)
+
+@dataclass
+class Task:
+    var: str
+    dt: str
+    time_idx: int
 
 # Reset to a snapshot
 # [print(f"{snapshot.message}, snapshot_id: {snapshot.id}") for snapshot in repo.ancestry(branch="main")]
@@ -34,7 +42,8 @@ lon_slice = slice(-124.7, -124.5)
 # TODO:
 # - [x] test small number of files with virtual refs
 # - [x] test small number of files with zarr
-# - [ ] Write and test with validation function
+# - [x] Write and test with validation function
+# - [ ] Add command line argument for start and end datetime
 # - [ ] estimate time to run + memory requirements for larger batches
 # - [ ] run on larger batches of files
 # - [ ] Nice to have - replace micromamba with uv in Dockerfile for mur sst runtime
@@ -125,40 +134,38 @@ def concat_virtual_datasets(results):
     return combined_vds
 
 
-def write_virtual_results_to_icechunk(virtual_ds, start_date: str, end_date: str):
+def write_virtual_results_to_icechunk(virtual_ds, start_date: str, end_date: str, append_dim: str = None):
     repo = open_or_create_repo()
     session = repo.writable_session("main")
 
     # Check if store is already populated
     with session.allow_pickling():
-        try:
-            # Try to access any of the expected data variables to check if store is populated
-            store_has_data = any(var in session.store for var in data_vars)
+        if append_dim:
             # Only use append_dim if store already has data
             virtual_ds.virtualize.to_icechunk(
-                session.store, append_dim="time" if store_has_data else None
+                session.store, append_dim=append_dim
             )
-        except Exception:
+        else:
             # If we can't check or there's an error, assume store is empty
             virtual_ds.virtualize.to_icechunk(session.store)
 
-        return session.commit(f"Commit data {start_date} to {end_date}")
+    return session.commit(f"Commit data {start_date} to {end_date}")
 
 
-def concat_and_write_virtual_datasets(results, start_date: str, end_date: str):
+def concat_and_write_virtual_datasets(results, start_date: str, end_date: str, append_dim: str = None):
     """Reduce to concat virtual datasets and write to icechunk."""
     combined_vds = concat_virtual_datasets(results)
-    return write_virtual_results_to_icechunk(combined_vds, start_date, end_date)
+    return write_virtual_results_to_icechunk(combined_vds, start_date, end_date, append_dim)
 
 
 # Lithops wrapper funtion for virtual datasets
-def lithops_write_virtual_references(start_date: datetime, end_date: datetime):
+def lithops_write_virtual_references(start_date: datetime, end_date: datetime, append_dim: str = None):
     uris = list_mur_sst_files(start_date, end_date)
     fexec.map_reduce(
         map_function=map_open_virtual_dataset,
         map_iterdata=uris,
         reduce_function=concat_and_write_virtual_datasets,
-        extra_args_reduce=(start_date, end_date),
+        extra_args_reduce=(start_date, end_date, append_dim),
         # map_runtime_memory=
         # reduce_runtime_memory=
         spawn_reducer=100,
@@ -214,14 +221,6 @@ def xarray_open_mfdataset(files):
         {"time": 1, "lat": 1447, "lon": 2895}
     )
     return ds
-
-
-@dataclass
-class Task:
-    var: str
-    dt: str
-    time_idx: int
-
 
 def write_data_to_zarr(task: Task, session: icechunk.Session, ds: xr.Dataset):
     group = zarr.group(store=session.store, overwrite=False)
@@ -302,13 +301,13 @@ def lithops_write_zarr(start_date: str, end_date: str):
     return f"Wrote data to resized arrays, snapshot {snapshot_id}"
 
 
-def lithops_check_data_store_access(open_or_create_repo: callable):
+def lithops_check_data_store_access(open_or_create_repo: callable, start_date: str, end_date: str):
     repo = open_or_create_repo()
     session = repo.readonly_session("main")
     xds = xr.open_dataset(
         session.store, consolidated=False, zarr_format=3, engine="zarr"
     )
-    return xds["analysed_sst"].sel(lat=lat_slice, lon=lon_slice).mean().values
+    return xds["analysed_sst"].sel(time=slice(start_date, end_date), lat=lat_slice, lon=lon_slice).mean().values
 
 
 def open_and_read_data(file, lat_slice, lon_slice):
@@ -325,17 +324,11 @@ def lithops_check_original_files(
 ):
     # map open and read data from selected space and time
     files = list_mur_sst_files(start_date, end_date, dmrpp=False)
-    # fexec.map_reduce(
-    #     map_function=open_and_read_data, # return array
-    #     map_iterdata=files,
-    #     extra_args=(lat_slice, lon_slice),
-    #     reduce_function=get_mean,
-    # )
-    fexec.map(
-        map_function=open_and_read_data,  # return array
+    fexec.map_reduce(
+        map_function=open_and_read_data, # return array
         map_iterdata=files,
         extra_args=(lat_slice, lon_slice),
-        # reduce_function=get_mean,
+        reduce_function=get_mean,
     )
     return fexec.get_result()
 
@@ -357,40 +350,53 @@ date_process_dict = {
     ("2023-04-23", "2023-09-03"): "virtual_dataset",
 }
 
-test_date_process_dict = {
-    ("2002-07-06", "2002-07-07"): "zarr",
-}
+# Convert dictionary to a Pandas DataFrame with IntervalIndex
+interval_df = pd.DataFrame([
+    {"interval": pd.Interval(pd.Timestamp(start), pd.Timestamp(end), closed="both"), "label": label}
+    for (start, end), label in date_process_dict.items()
+])
 
-fexec = lithops.FunctionExecutor(config_file="lithops.yaml", log_level=logging.INFO)
-
+# Function to find the corresponding label for two dates
+def find_label_for_range(date_str1, date_str2, df):
+    date1, date2 = pd.Timestamp(date_str1), pd.Timestamp(date_str2)
+    
+    # Find intervals where both dates are contained
+    match = df[df["interval"].apply(lambda interval: date1 in interval and date2 in interval)]
+    if match.empty:
+        raise ValueError(f"No matching interval found for dates {date_str1} and {date_str2}")
+    
+    return match["label"].iloc[0] if not match.empty else None
 
 # Wrapper functions for calling lithops
 ## Main function - write to icechunk
-def write_to_icechunk():
-    for k, v in test_date_process_dict.items():
-        if v == "virtual_dataset":
-            result = lithops_write_virtual_references(start_date=k[0], end_date=k[1])
-            print(result)
-        elif v == "zarr":
-            result = lithops_write_zarr(start_date=k[0], end_date=k[1])
-            print(result)
+def write_to_icechunk(start_date: str, end_date: str, append_dim: str = None):
+    # Find which process is associated with start and end date
+    # later, we could loop through the dict
+    # for dates, process in date_process_dict.items():
+    process = find_label_for_range(start_date, end_date, interval_df)
+    if process == "virtual_dataset":
+        result = lithops_write_virtual_references(start_date=start_date, end_date=end_date, append_dim=append_dim)
+        print(result)
+    elif process == "zarr":
+        result = lithops_write_zarr(start_date=start_date, end_date=end_date)
+        print(result)
     return "Done"
 
 
 ## Test data store access
-def check_data_store_access():
+def check_data_store_access(start_date: str, end_date: str):
     fexec.call_async(
         func=lithops_check_data_store_access,
-        data={"open_or_create_repo": open_or_create_repo},
+        data=dict(open_or_create_repo=open_or_create_repo, start_date=start_date, end_date=end_date),
     )
     print(fexec.get_result())
 
 
 ## Test original files
-def check_original_files():
+def check_original_files(start_date: str, end_date: str):
     result = lithops_check_original_files(
-        start_date="2002-06-30",
-        end_date="2002-07-07",
+        start_date=start_date,
+        end_date=end_date,
         lat_slice=lat_slice,
         lon_slice=lon_slice,
     )
@@ -425,17 +431,35 @@ def parse_args():
         ],
         help="The function to run.",
     )
+    parser.add_argument(
+        "--start_date",
+        type=str,
+        help="Start date for data processing (YYYY-MM-DD).",
+    )
+    parser.add_argument(
+        "--end_date",
+        type=str,
+        help="End date for data processing (YYYY-MM-DD).",
+    )
+    parser.add_argument(
+        "--append_dim",
+        type=str,
+        help="Append dimension for writing to icechunk.",
+    )    
     return parser.parse_args()
 
 
 if __name__ == "__main__":
     args = parse_args()
+    start_date = args.start_date
+    end_date = args.end_date
+    append_dim = args.append_dim
 
     if args.function == "write_to_icechunk":
-        write_to_icechunk()
+        write_to_icechunk(start_date=start_date, end_date=end_date, append_dim=append_dim)
     elif args.function == "check_data_store_access":
-        check_data_store_access()
+        check_data_store_access(start_date=start_date, end_date=end_date)
     elif args.function == "check_original_files":
-        check_original_files()
+        check_original_files(start_date=start_date, end_date=end_date)
     elif args.function == "list_installed_packages":
         list_installed_packages()
