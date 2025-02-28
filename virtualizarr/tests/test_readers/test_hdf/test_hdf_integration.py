c461b3310b9ff9b4e1bb7fadd49ef12de7d13832
Sean Harkins <sharkinsgis@gmail.com>
2025-02-28 17:12:14 -0500

    Align HDF reader CF _FillValue with Zarr v3 semantics (#420)
    * Remove unneccesary h5py fillvalue extraction logic.
    
    * Convert CF _FillValue attr to variable encoding for HDF files.
    
    * Handle xarray zarr backend decoding logic.
    
    * Include temporary broken integration tests for _FillValue.
    
    * Include _FillValue encoding intergration tests for HDFVirtualBackend.

--- virtualizarr/tests/test_readers/test_hdf/test_hdf_integration.py
+++ virtualizarr/tests/test_readers/test_hdf/test_hdf_integration.py
@@ -6,9 +6,11 @@ import virtualizarr
 from virtualizarr.readers.hdf import HDFVirtualBackend
 from virtualizarr.tests import (
     requires_hdf5plugin,
+    requires_icechunk,
     requires_imagecodecs,
     requires_kerchunk,
 )
+from virtualizarr.tests.test_integration import roundtrip_as_in_memory_icechunk
 
 
 @requires_kerchunk
@@ -53,8 +55,12 @@ class TestIntegration:
         vds.virtualize.to_kerchunk(kerchunk_file, format="json")
         roundtrip = xr.open_dataset(kerchunk_file, engine="kerchunk")
         xrt.assert_allclose(ds, roundtrip)
+        assert (
+            ds["temperature"].encoding["_FillValue"]
+            == roundtrip["temperature"].encoding["_FillValue"]
+        )
 
-    def test_non_coord_dim(self, tmpdir, non_coord_dim):
+    def test_non_coord_dim_roundtrip(self, tmpdir, non_coord_dim):
         ds = xr.open_dataset(non_coord_dim)
         vds = virtualizarr.open_virtual_dataset(
             non_coord_dim, backend=HDFVirtualBackend
@@ -62,4 +68,19 @@ class TestIntegration:
         kerchunk_file = f"{tmpdir}/kerchunk.json"
         vds.virtualize.to_kerchunk(kerchunk_file, format="json")
         roundtrip = xr.open_dataset(kerchunk_file, engine="kerchunk")
+        xrt.assert_equal(ds, roundtrip)
+
+    @requires_icechunk
+    def test_cf_fill_value_roundtrip(self, tmpdir, cf_fill_value_hdf5_file):
+        ds = xr.open_dataset(cf_fill_value_hdf5_file, engine="h5netcdf")
+        if ds["data"].dtype in [float, object]:
+            pytest.xfail(
+                "To fix handle fixed-length and structured type fill value \
+                encoding in xarray zarr backend."
+            )
+        vds = virtualizarr.open_virtual_dataset(
+            cf_fill_value_hdf5_file,
+            backend=HDFVirtualBackend,
+        )
+        roundtrip = roundtrip_as_in_memory_icechunk(vds, tmpdir, decode_times=False)
         xrt.assert_equal(ds, roundtrip)
