7f25ec8283dbcab50e693209e8f7731a88bd880b
Tom Nicholas <tom@earthmover.io>
2025-03-02 01:42:38 -0500

    add method for getting array metadata

--- virtualizarr/store.py
+++ virtualizarr/store.py
@@ -5,6 +5,15 @@ import xarray as xr
 from fsspec.asyn import AsyncFileSystem
 from zarr.abc.store import RangeByteRequest, Store
 from zarr.core.buffer import Buffer, BufferPrototype
+from zarr.metadata import ArrayV3Metadata
+
+from virtualizarr.manifests import ManifestArray
+
+
+def validate_dataset_is_fully_virtual(vds: xr.Dataset) -> None:
+    for name, var in vds.variables.items():
+        if not isinstance(var.data, ManifestArray):
+            raise TypeError(f"All variables in the dataset must be virtual, but variable {name} is of type {type(var.data)}, not type `ManifestArray`")
 
 
 class ManifestStore(Store):
@@ -14,12 +23,26 @@ class ManifestStore(Store):
     supports_listing: bool = True
 
     fs: AsyncFileSystem
+    # TODO probably this should be dict[path, Dataset] to cover groups, but not enforce coordinate inheritance like xarray.DataTree would.
     vds: xr.Dataset
 
     def __init__(self, fs, vds, read_only=True):
+
+        # TODO do we need to specify that this is v3 only?
+
         super().__init__(read_only=read_only)
-        self.fs = fs
-        self.vds = vds
+        self._fs = fs
+
+        validate_dataset_is_fully_virtual(vds)
+        self._vds = vds
+
+    def vds(self) -> xr.Dataset:
+        """The Virtual Dataset whose byte references this store reads from."""
+        return self._vds
+
+    def fs(self) -> AsyncFileSystem:
+        """The Fsspec FileSystem the byte ranges are defined for."""
+        return self._fs
 
     async def clear(self) -> None:
         self.fs = None
@@ -44,8 +67,16 @@ class ManifestStore(Store):
         prototype: BufferPrototype,
         byte_range: None = None,  # could this optionally accept a RangeByteRequest?
     ) -> Buffer | None:
+        
         if not self._is_open:
             await self._open()
+
+        if key.endswith('zarr.json'):
+            array_path = key.removesuffix('zarr.json')
+            metadata = self.get_array_metadata(array_path)
+            # I don't understand why there is a single-element dict to unpack here...
+            return metadata.to_buffer_dict["zarr.json"]
+
         print("key: ", key)
         print("key split: ", key.split("/"))
         array_name, _, chunk_key = key.split("/")
@@ -60,8 +91,15 @@ class ManifestStore(Store):
         )
         return value
 
-    # TODO: need a get_v3_array_metadata method
-    # to handle key="zarr.json" and return the metadata for the array
+    def get_array_metadata(self, array_path: str) -> ArrayV3Metadata:
+        *groups, array_name = array_path.split('/')
+        
+        if groups:
+            # TODO support groups via dict[path, Dataset]
+            raise NotImplementedError()
+        else:
+            var = self.vds.variables[array_name]
+            return var.data.metadata
 
     async def get_partial_values(
         self,
