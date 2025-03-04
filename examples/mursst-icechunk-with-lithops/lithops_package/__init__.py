3f2bfe44b95edb8aa9877925416dd00d0da19de7
Aimee Barciauskas <aimee@developmentseed.org>
2025-03-04 09:03:16 -0800

    Refactor lithops code and add README and code for EC2 builder

---
+++ examples/mursst-icechunk-with-lithops/lithops_package/__init__.py
@@ -0,0 +1,26 @@
+"""
+Lithops package for MUR SST data processing import
+
+This package provides functionality for processing MUR SST data using Lithops,
+a framework for serverless computing import
+"""
+
+from lithops_package import (
+    config,
+    data_processing,
+    lithops_wrappers,
+    models,
+    repo,
+    url_utils,
+    virtual_datasets,
+)
+
+__all__ = [
+    "config",
+    "data_processing",
+    "lithops_wrappers",
+    "models",
+    "repo",
+    "url_utils",
+    "virtual_datasets",
+]
