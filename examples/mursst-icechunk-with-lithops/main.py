83f9be892959bf297b110ad249194e1a9c8eb71d
Aimee Barciauskas <aimee@developmentseed.org>
2025-03-07 11:26:11 -0800

    Add mursst lithops example (#475)
    * Add files
    
    * Refactoring some things
    
    * Fix some refactoring things
    
    * Update PR number
    
    * Use configure_zarr in helpers
    
    * Remove some env specific configuration in lithops.yaml

---
+++ examples/mursst-icechunk-with-lithops/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Main entry point for the lithops package.
+
+This script provides a simple interface for running the package from the command line.
+"""
+
+from cli import main
+
+if __name__ == "__main__":
+    main()
