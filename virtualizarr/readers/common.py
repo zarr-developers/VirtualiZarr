cc34f772b71d3ebf4150af878bec1417cf5608f6
Tom Nicholas <tom@earthmover.io>
2025-03-10 16:03:50 -0400

    implement logic to load 1D dimension coords by default

--- virtualizarr/readers/common.py
+++ virtualizarr/readers/common.py
@@ -60,24 +60,25 @@ def replace_virtual_with_loadable_vars(
     )
 
     if isinstance(loadable_variables, list):
-        print(loadable_variables)
-        print(loadable_ds)
-        # this will automatically keep any IndexVariables needed for loadable 1D coordinates
-        ds_loadable_to_keep = loadable_ds[loadable_variables]
-        print(ds_loadable_to_keep)
-
-        ds_virtual_to_keep = fully_virtual_dataset.drop_vars(
-            loadable_variables, errors="ignore"
-        )
-        print(ds_virtual_to_keep)
+        var_names_to_load: list[str] = loadable_variables
     elif loadable_variables is None:
-        # TODO if loadable_variables is None then we have to explicitly match default behaviour of xarray
-        # i.e. load and create indexes only for dimension coordinate variables
-        raise NotImplementedError()
+        # If `loadable_variables`` is None then we have to explicitly match default behaviour of xarray
+        # i.e. load and create indexes only for dimension coordinate variables.
+        # We already have all the indexes and variables we should be keeping - we just need to distinguish them.
+        var_names_to_load: list[str] = [name for name, var in loadable_ds.variables.items() if var.dims == (name,)]
     else:
-        raise ValueError()
+        raise ValueError(f"loadable_variables must be an iterable of string variable names, or None, but got type {type(loadable_variables)}")
 
+    print(loadable_variables)
+    print(loadable_ds)
+    # this will automatically keep any IndexVariables needed for loadable 1D coordinates
+    ds_loadable_to_keep = loadable_ds[var_names_to_load]
+    print(ds_loadable_to_keep)
 
+    ds_virtual_to_keep = fully_virtual_dataset.drop_vars(
+        var_names_to_load, errors="ignore"
+    )
+    print(ds_virtual_to_keep)
 
     return xr.merge(
         [
