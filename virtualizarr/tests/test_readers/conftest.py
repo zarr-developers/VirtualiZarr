7410544eaff8d3ed3019c4d9564c09fa15f93e44
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-03-31 17:52:28 -0400

    Consolidate test fixtures

--- virtualizarr/tests/test_readers/conftest.py
+++ virtualizarr/tests/test_readers/conftest.py
@@ -17,6 +17,15 @@ except ModuleNotFoundError:
 
 
 @pytest.fixture
+def geotiff_file(tmp_path: Path) -> str:
+    """Create a NetCDF4 file with air temperature data."""
+    filepath = tmp_path / "air.tif"
+    with xr.tutorial.open_dataset("air_temperature") as ds:
+        ds.isel(time=0).rio.to_raster(filepath)
+    return str(filepath)
+
+
+@pytest.fixture
 def empty_chunks_hdf5_file(tmpdir):
     ds = xr.Dataset({"data": []})
     filepath = f"{tmpdir}/empty_chunks.nc"
