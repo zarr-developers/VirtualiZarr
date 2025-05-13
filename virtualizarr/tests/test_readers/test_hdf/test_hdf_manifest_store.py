d406ecba19288cf5c8fcd1091cafa5449893de6c
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-05-13 19:26:45 -0400

    Test roundtripping HDF5 with partial chunk
    Co-authored-by: Tom Nicholas <tom@earthmover.io>

--- virtualizarr/tests/test_readers/test_hdf/test_hdf_manifest_store.py
+++ virtualizarr/tests/test_readers/test_hdf/test_hdf_manifest_store.py
@@ -40,6 +40,22 @@ class TestHDFManifestStore:
         )
         xr.testing.assert_allclose(basic_ds, rountripped_ds)
 
+    def test_rountrip_partial_chunk_virtualdataset(self, tmpdir, basic_ds):
+        "Roundtrip a dataset to/from NetCDF with the HDF reader and ManifestStore with a single partial chunk"
+
+        filepath = f"{tmpdir}/basic_ds_roundtrip.nc"
+        encoding = {
+            "temperature": {"chunksizes": (90, 90), "original_shape": (100, 100)}
+        }
+        basic_ds.to_netcdf(filepath, engine="h5netcdf", encoding=encoding)
+        store = HDFVirtualBackend._create_manifest_store(
+            filepath=filepath,
+        )
+        rountripped_ds = xr.open_dataset(
+            store, engine="zarr", consolidated=False, zarr_format=3
+        )
+        xr.testing.assert_allclose(basic_ds, rountripped_ds)
+
     def test_rountrip_simple_virtualdataset_default_store(self, tmpdir, basic_ds):
         "Roundtrip a dataset to/from NetCDF with the HDF reader and ManifestStore"
 
