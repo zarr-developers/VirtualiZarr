f3e3b028b58cf78b7c4a9fead1318ac50edcd4ad
sharkinsspatial <sharkinsgis@gmail.com>
2025-06-16 15:12:12 -0400

    Remove json specification determination for kerchunk json parser.

--- virtualizarr/parsers/kerchunk_json.py
+++ virtualizarr/parsers/kerchunk_json.py
@@ -64,35 +64,25 @@ class Parser:
 
         reader = ObstoreReader(store=object_store, path=file_url)
 
-        # JSON has no magic bytes, but the Kerchunk version 1 spec starts with 'version':
-        # https://fsspec.github.io/kerchunk/spec.html
-        error_message = "The input Kerchunk reference did not seem to be in Kerchunk's JSON or Parquet spec: https://fsspec.github.io/kerchunk/spec.html. If your Kerchunk generated references are saved in parquet format, make sure the file extension is `.parquet`. The Kerchunk format autodetection is quite flaky, so if your reference matches the Kerchunk spec feel free to open an issue: https://github.com/zarr-developers/VirtualiZarr/issues"
-        try:
-            has_version = reader.read(9).startswith(b'{"version')
-        except OSError:
-            raise ValueError(error_message)
-        if has_version:
-            reader.seek(0)
-            content = reader.readall().decode()
-            refs = ujson.loads(content)
-            if self.store_registry is None:
-                unique_paths = {
-                    v[0]
-                    for v in refs["refs"].values()
-                    if isinstance(v, list) and isinstance(v[0], str)
-                }
-                stores = {}
-                for path in unique_paths:
-                    stores[get_store_prefix(path)] = object_store
-                registry = ObjectStoreRegistry(stores=stores)
-            else:
-                registry = self.store_registry
-            manifestgroup = manifestgroup_from_kerchunk_refs(
-                refs,
-                group=self.group,
-                fs_root=self.fs_root,
-                skip_variables=self.skip_variables,
-            )
-            return ManifestStore(group=manifestgroup, store_registry=registry)
+        reader.seek(0)
+        content = reader.readall().decode()
+        refs = ujson.loads(content)
+        if self.store_registry is None:
+            unique_paths = {
+                v[0]
+                for v in refs["refs"].values()
+                if isinstance(v, list) and isinstance(v[0], str)
+            }
+            stores = {}
+            for path in unique_paths:
+                stores[get_store_prefix(path)] = object_store
+            registry = ObjectStoreRegistry(stores=stores)
         else:
-            raise ValueError(error_message)
+            registry = self.store_registry
+        manifestgroup = manifestgroup_from_kerchunk_refs(
+            refs,
+            group=self.group,
+            fs_root=self.fs_root,
+            skip_variables=self.skip_variables,
+        )
+        return ManifestStore(group=manifestgroup, store_registry=registry)
