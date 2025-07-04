6cd8a4fef117c7e2133a5950541afc56a82b4c90
Tom Nicholas <tom@earthmover.io>
2025-07-03 23:03:09 -0400

    test warning raised if writing non-virtual dataset to icechunk

--- virtualizarr/tests/test_writers/test_icechunk.py
+++ virtualizarr/tests/test_writers/test_icechunk.py
@@ -473,6 +473,12 @@ def test_roundtrip_coords(
     assert set(roundtrip.coords) == set(vds.coords)
 
 
+def test_warn_if_no_virtual_vars(icechunk_filestore: "IcechunkStore"):
+    non_virtual_ds = xr.Dataset({"foo": ("x", [10, 20, 30]), "x": ("x", [1, 2, 3])})
+    with pytest.warns(UserWarning, match="non-virtual"):
+        non_virtual_ds.virtualize.to_icechunk(icechunk_filestore)
+
+
 class TestAppend:
     """
     Tests for appending to existing icechunk store.
