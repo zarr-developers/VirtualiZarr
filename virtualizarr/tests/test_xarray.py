3e6278f297ba770b1ef2793fa620f72086c91624
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-07-04 14:59:24 -0400

    Allow concatenating empty variables (#641)
    * Allow concatenating empty variables
    
    * Add test
    
    * add test for indexing scalar with an ellipsis
    
    * simplify test
    
    * release note
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add note about test being a regression test
    
    ---------
    
    Co-authored-by: Tom Nicholas <tom@earthmover.io>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- virtualizarr/tests/test_xarray.py
+++ virtualizarr/tests/test_xarray.py
@@ -878,3 +878,12 @@ def test_drop_variables(netcdf4_file):
         file_url=netcdf4_file, object_store=store, parser=parser, drop_variables=["air"]
     ) as vds:
         assert "air" not in vds.variables
+
+
+def test_concat_zero_dimensional_var(manifest_array):
+    # regression test for https://github.com/zarr-developers/VirtualiZarr/pull/641
+    marr = manifest_array(shape=(), chunks=())
+    vds1 = xr.Dataset({"a": marr})
+    vds2 = xr.Dataset({"a": marr})
+    result = xr.concat([vds1, vds2], dim="time", coords="minimal", compat="override")
+    assert result["a"].sizes == {"time": 2}
