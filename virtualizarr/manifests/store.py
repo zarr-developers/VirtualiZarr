1e632c3eb97632e435819639805f57cbe382fc0f
Max Jones <14077947+maxrjones@users.noreply.github.com>
2025-08-09 13:08:51 -0400

    Always strip preceding / from path in ManifestStore (#764)
    * Always strip preceding / from path in ManifestStore
    
    * Fix docs error

--- virtualizarr/manifests/store.py
+++ virtualizarr/manifests/store.py
@@ -195,10 +195,11 @@ class ManifestStore(Store):
         path_in_store = urlparse(path).path
         if hasattr(store, "prefix") and store.prefix:
             prefix = str(store.prefix).lstrip("/")
-            path_in_store = path_in_store.lstrip("/").removeprefix(prefix).lstrip("/")
         elif hasattr(store, "url"):
             prefix = urlparse(store.url).path.lstrip("/")
-            path_in_store = path_in_store.lstrip("/").removeprefix(prefix).lstrip("/")
+        else:
+            prefix = ""
+        path_in_store = path_in_store.lstrip("/").removeprefix(prefix).lstrip("/")
         # Transform the input byte range to account for the chunk location in the file
         chunk_end_exclusive = offset + length
         byte_range = _transform_byte_range(
