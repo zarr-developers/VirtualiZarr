196f9b186807b62f11cdb37618ac2c47b0f31620
Miguel Jimenez-Urias <mjimenez@opendap.org>
2026-03-04 17:03:03 -0800

    update DMRParser class to allow for on-the-fly dmrpps

--- virtualizarr/parsers/dmrpp.py
+++ virtualizarr/parsers/dmrpp.py
@@ -1,13 +1,22 @@
 import io
+import warnings
+from pathlib import Path
 from typing import Iterable
 from xml.etree import ElementTree as ET
 
+from obspec_utils.protocols import ReadableStore
 from obspec_utils.readers import EagerStoreReader
 from obspec_utils.registry import ObjectStoreRegistry
+from pydap.parsers.dmr import DMRPPParser as _DMRPPParser
 
 from virtualizarr.manifests import (
+    ChunkManifest,
+    ManifestArray,
+    ManifestGroup,
     ManifestStore,
 )
+from virtualizarr.manifests.utils import create_v3_array_metadata
+from virtualizarr.parsers.utils import encode_cf_fill_value
 
 
 class DMRPPParser:
@@ -56,9 +65,15 @@ class DMRPPParser:
         file_bytes = reader.readall()
         stream = io.BytesIO(file_bytes)
 
+        url = (
+            url.removesuffix(".dap.dmrpp")
+            if url.endswith(".dap.dmrpp")
+            else url.removesuffix(".dmrpp")
+        )
+
         parser = DMRParser(
             root=ET.parse(stream).getroot(),
-            data_filepath=url.removesuffix(".dmrpp"),
+            data_filepath=url,
             skip_variables=self.skip_variables,
         )
         manifest_store = parser.parse_dataset(object_store=store, group=self.group)
@@ -102,6 +117,86 @@ class DMRParser:
             If None, the data file path is taken from the DMR++ file.
         """
         self.root = root
-        self._validation_issues: list[str] = []
-        self.data_filepath = data_filepath
+        self.data_filepath = (
+            data_filepath if data_filepath is not None else self.root.attrib["name"]
+        )
         self.skip_variables = skip_variables or ()
+
+    def dmrparser(self) -> _DMRPPParser:
+        """Exposes the _DMRParser to external use (avoids breaking changes)"""
+        return _DMRPPParser(
+            root=self.root,
+            data_filepath=self.data_filepath,
+            skip_variables=self.skip_variables,
+        )
+
+    def parse_dataset(
+        self,
+        object_store: ReadableStore,
+        group: str | None = None,
+    ) -> ManifestStore:
+        """
+        Parses the given file and creates a ManifestStore.
+
+        Parameters
+        ----------
+        group
+            The group to parse. Ignored if no groups are present, and the entire
+            dataset is parsed. If `None` or "/", and groups are present, the first group
+            is parsed.  If not `None` or "/", and no groups are present, a UserWarning
+            is issued indicating that the group will be ignored.
+
+        Returns
+        -------
+        ManifestStore
+
+        Examples
+        --------
+        Open a sample DMR++ file and parse the dataset
+        """
+        group = group or "/"
+        ngroups = len(self.root.findall("dap:Group", self._NS))
+
+        if ngroups == 0 and group != "/":
+            warnings.warn(
+                f"No groups in DMR++ file {self.data_filepath!r}; "
+                f"ignoring group parameter {group!r}"
+            )
+
+        group_path = Path("/") if ngroups == 0 else Path("/") / group.removeprefix("/")
+
+        dataset_element = self.dmrparser()._split_groups(self.root).get(group_path)
+
+        if dataset_element is None:
+            raise ValueError(
+                f"Group {group_path} not found in DMR++ file {self.data_filepath!r}"
+            )
+
+        # get two dictionary containing relevant metadata
+        vars_dict, attrs = self.dmrparser()._parse_dataset(dataset_element)
+
+        manifest_dict: dict[str, ManifestArray] = {}
+
+        for var in vars_dict.keys():
+            chunkmanifest = ChunkManifest(vars_dict[var].pop("chunkmanifest", None))
+            meta = dict(
+                [
+                    (k, v)
+                    for k, v in vars_dict[var].items()
+                    if k not in ["Maps", "fqn_dims"]
+                ]
+            )
+            if "_FillValue" in meta["attributes"]:
+                encoded_cf_fill_value = encode_cf_fill_value(
+                    meta["attributes"]["_FillValue"], meta["data_type"]
+                )
+                meta["attributes"]["_FillValue"] = encoded_cf_fill_value
+            metadata = create_v3_array_metadata(**meta)
+            manifest_dict[var] = ManifestArray(
+                metadata=metadata, chunkmanifest=chunkmanifest
+            )
+        manifest_group = ManifestGroup(arrays=manifest_dict, attributes=attrs)
+        registry = ObjectStoreRegistry()
+        registry.register(self.data_filepath, object_store)
+
+        return ManifestStore(registry=registry, group=manifest_group)
