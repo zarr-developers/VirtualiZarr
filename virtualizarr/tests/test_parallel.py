3287d82f60cee363774e8f2750d3f1cf27685b59
Chuck Daniels <chuck@developmentseed.org>
2026-03-06 12:12:52 -0500

    Support functools.partial with lithops (#899)
    * Run doctests via pytest
    * Support use of functools.partial with Lithops.
    * Use mode "forkserver" with ProcessPoolExecutor if the
      default mode is "fork." This can be removed once our minimum python
      version is 3.14, as "fork" will no longer be a default mode.

---
+++ virtualizarr/tests/test_parallel.py
@@ -0,0 +1,43 @@
+import multiprocessing as mp
+
+import pytest
+
+from virtualizarr.parallel import LithopsEagerFunctionExecutor, get_executor
+from virtualizarr.tests import requires_lithops
+
+
+@requires_lithops
+def test_lithops_executor_with_multiple_args():
+    with LithopsEagerFunctionExecutor() as exec:
+        results = exec.map(lambda x, y: x + y, (1, 2, 3), (4, 5, 6))
+
+    assert tuple(results) == (5, 7, 9)
+
+
+@requires_lithops
+def test_lithops_executor_with_partial():
+    from functools import partial
+
+    inc = partial(lambda x, y: x + y, 1)
+
+    with LithopsEagerFunctionExecutor() as exec:
+        results = exec.map(inc, (1, 2, 3))
+
+    assert tuple(results) == (2, 3, 4)
+
+
+@pytest.mark.skipif(
+    mp.get_start_method() != "fork",
+    reason="Default multiprocessing start method is not 'fork'",
+)
+def test_get_executor_process_pool_mode():
+    from concurrent.futures import ProcessPoolExecutor
+
+    executor = get_executor(ProcessPoolExecutor)()
+
+    assert isinstance(executor, ProcessPoolExecutor), "Expected a ProcessPoolExecutor"
+
+    ctx = executor._mp_context
+
+    assert ctx is not None, "Expected executor to have a multiprocessing context"
+    assert ctx.get_start_method() == "forkserver"
